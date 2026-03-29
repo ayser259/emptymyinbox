1aa95127088607886be851455c9a367f792100ac
Ayser Choudhury <ayserchoudhury@gmail.com>
2026-03-29 12:00:06 -0700

    Created Skeleton mac app

---
+++ Mac/emptymyinboxMacApp/emptymyinboxMacApp/emptymyinboxMacAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  emptymyinboxMacAppApp.swift
+//  emptymyinboxMacApp
+//
+//  Created by Ayser Choudhury on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct emptymyinboxMacAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
